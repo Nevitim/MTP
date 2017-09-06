2a20d2e524233221e48f601ed15b4745ce62352a
Neviton <nevitoncesar2009@hotmail.com>
2017-09-06 09:08:50 -0300

    x

---
+++ aula20170830/prg0.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int conv(char**str)
+	int x=0;
+	int i=0;
+	while(str[i])
+	{
+	x=x*10+str[i]-48;
+	i++;
+	}
+int main(int argc, char ** argv)
+{
+	printf(Soma= %d\n, conv(argv[1])+conv(argv[2]));
+	neturn 0;
+}
