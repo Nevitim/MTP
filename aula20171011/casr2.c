75f72daa0dbf8479d0844e85fa95cf1f4cb6dbcf
Neviton <nevitoncesar2009@hotmail.com>
2017-10-12 15:15:59 -0300

    fim de aula

---
+++ aula20171011/casr2.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#define n 8
+
+
+int main()
+{
+    char str[n];
+    str[n]='b';
+    printf("%c oiijoj",str);
+    printf("\n");
+    return EXIT_SUCCESS;
+}
