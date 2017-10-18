ef449da5b5727a50022d13a3f889a18d82ac91b7
Neviton <nevitoncesar2009@hotmail.com>
2017-10-18 08:42:01 -0200

    fim de aula

--- aula20171004/ptr7.c
+++ aula20171004/ptr7.c
@@ -1,23 +1,18 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#define n 4
+
 int main()
 {
-    int str[n]={0};
-    char * varredura=(char*)str;
-    int i=0;
-    for(i=0;i<n;i++)
-       {
-           printf("Entre com um numero: \n");
-            scanf("%i", &str[i]);
-       }
-    for (i=0;i<n; i++)
-        printf( "End.:%p=>dado:(%s)\n",varredura+i,varredura[i]);
-    printf("codigo:\n");
-    for(i=0;i<n;i++)
-        printf("%s", varredura[i]);
-    printf("\n");
-    return EXIT_SUCCESS;
-}
+    int vet[4]={0,0,0,0},i,j,let;
+    char *ptr;
+    for(i=0,ptr=(char*)vet;i<4;i++)
+    {
+        printf("\nDigite quatro numeros:\n");
+        scanf("%d",&vet[i]);
+    }
 
+        printf("Corresponde a:");
+        printf("%s",ptr);
+
+
+
+    }
