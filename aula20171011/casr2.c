be99157986ef3cb786e420a0996c5a29c3ab978d
Neviton <nevitoncesar2009@hotmail.com>
2017-10-22 12:02:40 -0200

    fim de aula

--- aula20171011/casr2.c
+++ aula20171011/casr2.c
@@ -1,16 +1,33 @@
-#include <stdlib.h>
-#include <ctype.h>
 #include <stdio.h>
-#include <time.h>
+#include <stdlib.h>
 #include <string.h>
-#define n 8
+#include <locale.h>
+#include <time.h>
 
+void printar(int l, int c)
+{
+    char olhaessamerda[] = { ": $ # $ : 4 b . ' :. : $ # $: 4b. ':."};
+    int i=0, j=0, n=0;
+    for(;i<l;i++)
+    {
+        printf("\n");
+        for(;j<c;j++)
+        {
+            n=rand()%38;
+            printf("%s", olhaessamerda[n]);
+        }
+    }
+    printf("\n");
+}
 
 int main()
 {
-    char str[n];
-    str[n]='b';
-    printf("%c oiijoj",str);
-    printf("\n");
+    srand(time(0));
+    setlocale(LC_ALL, "Portuguese");
+    int c=0, l=0;
+    printf("entre com o numero de linhas e colunas (l,c): ");
+    scanf("%d,%d", &l, &c);
+    getchar();
+    printar(l,c);
     return EXIT_SUCCESS;
 }
